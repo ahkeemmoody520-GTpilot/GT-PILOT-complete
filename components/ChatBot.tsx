279567e58b9ad627bdb9ccc508a00b199a24ca4a
ahkeemmoody520-GTpilot <ahkeemmoody520@gmail.com>
2025-11-10 00:12:30 -0500

    Create ChatBot.tsx

---
+++ components/ChatBot.tsx
@@ -0,0 +1,8 @@
+export default function ChatBot() {
+  return (
+    <div>
+      <h1>ChatBot Placeholder</h1>
+      <p>This component will handle GT Pilot’s chat interface.</p>
+    </div>
+  );
+}
