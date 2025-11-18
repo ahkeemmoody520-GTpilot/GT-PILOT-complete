30ea9973cd2b28e9e0143f0b2d358f7cbb545a0e
ahkeemmoody520-GTpilot <ahkeemmoody520@gmail.com>
2025-11-18 06:44:38 -0500

    Delete components directory

--- components/ChatBot.tsx
+++
@@ -1,8 +0,0 @@
-export default function ChatBot() {
-  return (
-    <div>
-      <h1>ChatBot Placeholder</h1>
-      <p>This component will handle GT Pilot’s chat interface.</p>
-    </div>
-  );
-}
